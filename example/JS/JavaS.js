356c2fc0356805bf6b54cbeed9629e5e951366a9
user <mariamalatrash1@gmail.com>
2020-03-14 09:26:23 +0200

    added my example html and css

---
+++ example/JS/JavaS.js
@@ -0,0 +1,23 @@
+function clickFunc()
+{
+	alert("this is alert from the function");
+}
+
+function pageLoaded()
+{
+	console.log("the page was loaded");
+}
+function myFunction() {
+  alert("You pressed a key inside the input field");
+}
+function mouseOver() {
+  document.getElementById("demo").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById("demo").style.color = "black";
+}
+console.log("Done:)");
+  function gotoPage(s) {
+            location.href = s;
+        }
